3fd5be57df832f1b60678cf9255deca55d08150c
Nilesh <nileshraut114@gmail.com>
2024-07-12 11:11:34 +0530

    header updating

--- src/Components/Dashboard/students/student.js
+++ src/Components/Dashboard/students/student.js
@@ -4,7 +4,7 @@ import Subjects from './Subjects.js'
 const Student = () => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false)
   return (
-   <div className={`${isSideNavOpen? 'ml-64': ''}`} >
+   <div className={`${isSideNavOpen? 'sm:ml-64': ''}`} >
    <Header isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen}/>
     <Subjects />
    </div>
