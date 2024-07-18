9a2cd3fc69d76be095415ab9cea4dedab4479bea
Nilesh <nileshraut114@gmail.com>
2024-07-18 16:56:55 +0530

    prettification of code

--- src/Components/Dashboard/students/Suggestion.js
+++ src/Components/Dashboard/students/Suggestion.js
@@ -1,23 +1,24 @@
-import React, { useState, useCallback } from 'react';
-import CommentEditor from './suggestion component/CommentEditor';
-import CommentList from './suggestion component/CommentList';
-import { useSelector } from 'react-redux';
+import React, { useState, useCallback } from "react";
+import CommentEditor from "./suggestion component/CommentEditor";
+import CommentList from "./suggestion component/CommentList";
+import { useSelector } from "react-redux";
 
 const Suggestion = ({ topicId }) => {
-    const [refreshTrigger, setRefreshTrigger] = useState(false);
-    const status=useSelector(store=>store.user.status)
-    const refreshComments = useCallback(() => {
-        setRefreshTrigger(prev => !prev);
-    }, []);
+  const [refreshTrigger, setRefreshTrigger] = useState(false);
+  const status = useSelector((store) => store.user.status);
+  const refreshComments = useCallback(() => {
+    setRefreshTrigger((prev) => !prev);
+  }, []);
 
-    return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Suggestion</h1>
-            { status &&           <CommentEditor topicId={topicId} refreshComments={refreshComments} />
-} 
-            <CommentList topicId={topicId} refreshTrigger={refreshTrigger} />
-       </div>
-    );
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Suggestion</h1>
+      {status && (
+        <CommentEditor topicId={topicId} refreshComments={refreshComments} />
+      )}
+      <CommentList topicId={topicId} refreshTrigger={refreshTrigger} />
+    </div>
+  );
 };
 
 export default Suggestion;
--- src/Components/Dashboard/students/particulartopics.js
+++ src/Components/Dashboard/students/particulartopics.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../Navbar/header'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
@@ -6,75 +6,75 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { IoIosArrowBack } from "react-icons/io";
 import Loading from '../../Loading/Loading'
-import  Suggestion  from './Suggestion.js'
+import Suggestion from './Suggestion.js'
 const Particulartopics = () => {
-  const navigate=useNavigate()
-  const {id}=useParams()
+  const navigate = useNavigate()
+  const { id } = useParams()
   const [isSideNavOpen, setIsSideNavOpen] = useState(false)
 
-  const [data,setdata]=useState()
+  const [data, setdata] = useState()
   console.log(data);
-  useEffect(()=>{
-    axios.get(`${process.env.REACT_APP_API_URL}/api/topics/${id}`).then(res=>{setdata(res.data.data);console.log(res.data.data);}).catch(err=>console.log(err))
-  },[id,navigate])
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/api/topics/${id}`).then(res => { setdata(res.data.data); console.log(res.data.data); }).catch(err => console.log(err))
+  }, [id, navigate])
   return (
-    <div className={`${isSideNavOpen? 'sm:ml-64': ''}`} >
-   <Header isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen}/>
-    {/* <div>Title: {data?.name}</div>
+    <div className={`${isSideNavOpen ? 'sm:ml-64' : ''}`} >
+      <Header isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen} />
+      {/* <div>Title: {data?.name}</div>
     <div>{data?.description}</div> */}
- 
-<div className='p-2'>
+
+      <div className='p-2'>
 
 
 
-<div class="m-2 font-semibold text-xl flex flex-row ">
-  <button className='px-2' onClick={()=>{navigate(-1)}}><IoIosArrowBack color='red' />
-  </button>
-  <p></p>
+        <div class="m-2 font-semibold text-xl flex flex-row ">
+          <button className='px-2' onClick={() => { navigate(-1) }}><IoIosArrowBack color='red' />
+          </button>
+          <p></p>
 
-</div>
+        </div>
 
-{data ? <div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6">
-      <h1 className="text-3xl font-bold mb-4">{data?.name}</h1>
-      <div className="mb-4">
-        <span className="font-semibold">Standard:</span> {data?.subject?.standard}
+        {data ? <div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6">
+          <h1 className="text-3xl font-bold mb-4">{data?.name}</h1>
+          <div className="mb-4">
+            <span className="font-semibold">Standard:</span> {data?.subject?.standard}
+          </div>
+          <div className="mb-4 text-gray-600">
+            <span className="font-semibold">Chapter:</span> {data?.chapter.name}
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Subject:</span> <Link to={`/subject/${data?.subject?._id}`}>{data?.subject?.name}</Link>
+          </div>
+          <div className="mb-4">
+            <span className="font-semibold">Topic Level:</span> {data?.topic_level}
+          </div>
+          <div className="mb-6">
+            <span className="font-semibold">Created At:</span> {new Date(data?.createdAt).toLocaleDateString()}
+          </div>
+          <div className="prose mb-6" dangerouslySetInnerHTML={{ __html: data?.description }}></div>
+          <div className="mb-6">
+            <h2 className="text-2xl font-semibold mb-2">Related Topic</h2>
+            <ul className="list-disc list-inside px-4">
+              {data?.RelatedTopic.map((topic) => (
+                <li key={topic._id}><a href={`/topic/${topic._id}`}>{topic.name}</a></li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold mb-2">Questions</h2>
+            <ul className="list-disc list-inside px-4">
+              {data?.questions.map((question) => (
+                <li key={question._id}>{question.questionText}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+          : <Loading />}
       </div>
-      <div className="mb-4 text-gray-600">
-        <span className="font-semibold">Chapter:</span> {data?.chapter.name}
-      </div>
-      <div className="mb-4">
-        <span className="font-semibold">Subject:</span> <Link to={`/subject/${data?.subject?._id}`}>{data?.subject?.name}</Link>
-      </div>
-      <div className="mb-4">
-        <span className="font-semibold">Topic Level:</span> {data?.topic_level}
-      </div>
-      <div className="mb-6">
-        <span className="font-semibold">Created At:</span> {new Date(data?.createdAt).toLocaleDateString()}
-      </div>
-      <div className="prose mb-6" dangerouslySetInnerHTML={{ __html: data?.description }}></div>
-      <div className="mb-6">
-        <h2 className="text-2xl font-semibold mb-2">Related Topic</h2>
-        <ul className="list-disc list-inside px-4">
-          {data?.RelatedTopic.map((topic) => (
-            <li key={topic._id}><a href={`/topic/${topic._id}`}>{topic.name}</a></li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2 className="text-2xl font-semibold mb-2">Questions</h2>
-        <ul className="list-disc list-inside px-4">
-          {data?.questions.map((question) => (
-            <li key={question._id}>{question.questionText}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-: <Loading/>}
-</div>
 
-<div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6"><Suggestion topicId={id}/></div>
+      <div className="max-w-2xl mx-auto bg-white shadow-md rounded-lg p-6"><Suggestion topicId={id} /></div>
 
-    </div>  )
+    </div>)
 }
 
 export default Particulartopics
