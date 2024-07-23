7519486878ee837a9af297b7da1ddd2a972e5e34
Nilesh <nileshraut114@gmail.com>
2024-07-23 10:13:01 +0530

    add test css

--- src/Components/Dashboard/students/test/Chaptertest.js
+++ src/Components/Dashboard/students/test/Chaptertest.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate,useParams } from 'react-router-dom'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Header from '../../../Navbar/header'
 import Loading from '../../../Loading/Loading'
+import { IoIosArrowBack } from "react-icons/io";
 export const Chaptertest = () => {
+  const navigate=useNavigate()
   const {Id}=useParams()
   const [test, setTest] = useState(null);
   const [isSideNavOpen, setIsSideNavOpen] = useState(false)
@@ -29,16 +31,32 @@ export const Chaptertest = () => {
    
      <div className={`${isSideNavOpen? 'sm:ml-64': ''}`} >
      <Header isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen}/>
+     <div className='p-2'>
     
+
+
+    <div className='flex flex-row justify-between '>
+    <div class="m-2 font-semibold text-xl flex flex-row ">
+      <button className='px-2' onClick={()=>{navigate(-1)}}><IoIosArrowBack color='red' />
+      </button>
+     
+      <p>Chapter Tets</p>
+    </div>
+    </div>
     
-    
+   
+
     {
       test ? test.map((data,index)=>{
         return (
-          <Link key={index} to={`/student/test/${data?._id}`}>{data?.testName}</Link>
+          <Link key={index} to={`/student/test/${data?._id}`}> <div class="flex justify-between p-2 m-2 border rounded-lg shadow-2xl">
+        <h1 class="pt-1">{data?.testName}</h1>
+    
+      </div></Link>
         )
       }) : <Loading/>
     }
+     </div>
      </div>
     </>
   )
