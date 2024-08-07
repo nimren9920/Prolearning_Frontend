924661864670ca767ef2743bf91244d4d864be17
Nilesh <nileshraut114@gmail.com>
2024-08-07 15:27:40 +0530

    fix the type support of file pdf

--- src/Components/Dashboard/students/test/physical test/Physicaltestupload.js
+++ src/Components/Dashboard/students/test/physical test/Physicaltestupload.js
@@ -203,6 +203,7 @@ formData.append('pdf', pdffile);
           className="flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full"
           type="file"
           id="pdf-upload"
+          accept="application/pdf"
         />
       </div>
    <button onClick={submittest} className="justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex items-center gap-2">
