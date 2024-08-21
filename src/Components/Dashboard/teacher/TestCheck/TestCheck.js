62471769aa9966011983eba0faa73ec5a622ffcb
Nilesh <nileshraut114@gmail.com>
2024-08-21 14:07:22 +0530

    simple changes

--- src/Components/Dashboard/teacher/TestCheck/TestCheck.js
+++ src/Components/Dashboard/teacher/TestCheck/TestCheck.js
@@ -153,9 +153,9 @@ const TestCheck = () => {
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-right"
                   placeholder="Score"
                   type="number"
-                  min="0"
+                  min={0}
                   max={question.score}
-                  value={responses[question._id] || ""}
+                  value={responses[question._id] || 0}
                   onChange={(e) =>
                     handleScoreChange(
                       question._id,
